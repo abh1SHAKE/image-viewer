31c566d95676a84ae22c1f6471e509feaba5b4c7
abh1SHAKE <abhshake17@gmail.com>
2026-01-16 00:02:26 +0530

    Render PPM images (P3 and P6) in SDL window

--- main.c
+++ main.c
@@ -4,38 +4,123 @@
 #define SDL_MAIN_HANDLED
 #include <SDL2/SDL.h>
 
-int main(int argc, char *argv[]) {
-    const int width = 900;
-    const int height = 600;
+#define MAX_WIN_W 1280
+#define MAX_WIN_H 720
 
-    printf("Call on me");
+int main(int argc, char *argv[]) {
+    FILE *in = stdin;
+    char line[1024];
+
+    // Read magic number (P3 - ASCII, P6 - Binary)
+    fgets(line, sizeof(line), in);
+
+    int is_p3 = 0, is_p6 = 0;
+    if (line[0] == 'P' && line[1] == '3') {
+        is_p3 = 1;
+    } else if (line[0] == 'P' && line[1] == '6') {
+        is_p6 = 1;
+    } else {
+        fprintf(stderr, "Unsupported PPM format\n");
+        return 1;
+    }
+
+    // Read until we get dimensions (skip comments)
+    do {
+        fgets(line, sizeof(line), in);
+    } while (line[0] == '#');
+
+    int width, height;
+    sscanf(line, "%d %d", &width, &height);
+
+    // Read maxval
+    do {
+        fgets(line, sizeof(line), in);
+    } while (line[0] == '#');
+
+    int maxval;
+    sscanf(line, "%d", &maxval);
+    if (maxval != 255) {
+        fprintf(stderr, "Only maxval 255 supported\n");
+        return 1;
+    }
+
+    // Clamp window size
+    int win_w = width > MAX_WIN_W ? MAX_WIN_W : width;
+    int win_h = height > MAX_WIN_H ? MAX_WIN_H : height;
+
+    float sx = (float) win_w / width;
+    float sy = (float) win_h / height;
+
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        fprintf(stderr, "SDL_Init failed: %s\n", SDL_GetError());
+        return 1;
+    }
 
     SDL_Window *pwindow = SDL_CreateWindow(
         "Image Viewer",
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
-        width, height,
+        win_w, win_h,
         0
     );
 
+    if (!pwindow) {
+        fprintf(stderr, "SDL_CreateWindow failed: %s\n", SDL_GetError());
+        return 1;
+    }
+
     SDL_Surface *psurface = SDL_GetWindowSurface(pwindow);
+    if (!psurface) {
+        fprintf(stderr, "SDL_GetWindowSurface failed: %s\n", SDL_GetError());
+        return 1;
+    }
 
-    Uint8 r, g, b;
-    r = 0x00;
-    g = 0x42;
-    b = 0x25;
-    Uint32 color = SDL_MapRGB(psurface -> format, r, g, b);
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            Uint8 r, g, b;
 
-    SDL_Rect pixel = (SDL_Rect){0, 0, 1, 1};
-    for (int x = 0; x < width; x++) {
-        for (int y = 0; y < height; y++) {
-            pixel.x = x;
-            pixel.y = y;
+            if (is_p3) {
+                int ri, gi, bi;
+                fscanf(in, "%d %d %d", &ri, &gi, &bi);
+                r = (Uint8) ri;
+                g = (Uint8) gi;
+                b = (Uint8) bi;
+            } else {
+                fread(&r, 1, 1, in);
+                fread(&g, 1, 1, in);
+                fread(&b, 1, 1, in);
+            }
+
+            Uint32 color = SDL_MapRGB(psurface -> format, r, g, b);
+            
+            SDL_Rect pixel = {
+                (int)(x * sx),
+                (int)(y * sy),
+                (int)(sx + 1),
+                (int)(sy + 1),
+            };
+
             SDL_FillRect(psurface, &pixel, color);
         }
     }
 
     SDL_UpdateWindowSurface(pwindow);
 
-    SDL_Delay(3000);
+    // App is running
+    int app_running = 1;
+    while (app_running) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                app_running = 0;
+            } 
+        }
+
+        SDL_UpdateWindowSurface(pwindow);
+        SDL_Delay(100);
+    }
+
+    SDL_DestroyWindow(pwindow);
+    SDL_Quit();
+    return 0;
 }
